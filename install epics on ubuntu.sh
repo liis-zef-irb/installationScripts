9af1f5ce093c5d970383e546c275c2fc66e46389
Lovro <lovro.basioli@irb.hr>
2025-02-17 13:52:28 +0100

    Update install epics on ubuntu.sh

--- install epics on ubuntu.sh
+++ install epics on ubuntu.sh
@@ -3,22 +3,22 @@ sudo apt-get install git-all build-essential cmake
 mkdir $HOME/EPICS
 cd $HOME/EPICS/
 git clone --recursive https://github.com/epics-base/epics-base.git base
-nano ~/.bashrc		add:	export EPICS_ROOT=${HOME}/EPICS
-				export EPICS_BASE=${EPICS_ROOT}/base
-				export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)
-				export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}
-				export SUPPORT=${EPICS_ROOT}/support
+
+echo "export EPICS_ROOT=${HOME}/EPICS" >> ~/.bashrc
+echo "export EPICS_BASE=${EPICS_ROOT}/base" >> ~/.bashrc
+echo "export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)" >> ~/.bashrc
+echo "export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}" >> ~/.bashrc
+echo "export SUPPORT=${EPICS_ROOT}/support" >> ~/.bashrc
 source ~/.bashrc
+
 cd ${EPICS_BASE}
 sudo make
 
-echo "SUPPORT=$SUPPORT" > configure/RELEASE
+echo "SUPPORT=$SUPPORT" >> configure/RELEASE
 echo '-include $(TOP)/configure/SUPPORT.$(EPICS_HOST_ARCH)' >> configure/RELEASE
 echo "EPICS_BASE=$EPICS_BASE" >> configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE' >> configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE.$(EPICS_HOST_ARCH)' >> configure/RELEASE
-echo "" >> configure/RELEASE
-echo "" >> configure/RELEASE
 
 cd ${EPICS_ROOT}
 wget https://raw.githubusercontent.com/liis-zef-irb/installationScripts/refs/heads/main/assemble_synApps
