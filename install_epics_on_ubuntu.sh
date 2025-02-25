3a4253411878711de460400ecf39c4110b121e12
Lovro <lovro.basioli@irb.hr>
2025-02-25 13:40:36 +0100

    possibly finished base and synapps, preparation for vcom driver

--- install_epics_on_ubuntu.sh
+++ install_epics_on_ubuntu.sh
@@ -1,34 +1,47 @@
-export SCRIPTS_DIR=$(pwd)
-export EPICS_ROOT=${HOME}/EPICS
-export EPICS_BASE=${EPICS_ROOT}/base
-export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)
-export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}
-export SUPPORT=${EPICS_ROOT}/support
+# Location of EPICS, feel free to change
+EPICS_ROOT=${HOME}/EPICS
 
+# Location of all installation scripts
+SCRIPTS_DIR=$(pwd)
+
+# Clone base repository from github
 mkdir $EPICS_ROOT
 cd $EPICS_ROOT
 git clone --recursive https://github.com/epics-base/epics-base.git base
 
-echo "export EPICS_ROOT=${HOME}/EPICS" >> ~/.bashrc
-echo "export EPICS_BASE=${EPICS_ROOT}/base" >> ~/.bashrc
-echo "export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)" >> ~/.bashrc
-echo "export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}" >> ~/.bashrc
-echo "export SUPPORT=${EPICS_ROOT}/support" >> ~/.bashrc
-source ~/.bashrc
+# Define enviroment variables for relevant folders
+EPICS_BASE=${EPICS_ROOT}/base
+EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)
+PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}
+SUPPORT=${EPICS_ROOT}/support
 
-sudo apt-get build-essential cmake
+# Auto-define on reboot
+echo "" >> ~/.bashrc
+echo "export EPICS_ROOT=${EPICS_ROOT}" >> ~/.bashrc
+echo "export EPICS_BASE=${EPICS_BASE}" >> ~/.bashrc
+echo "export EPICS_HOST_ARCH=${EPICS_HOST_ARCH}" >> ~/.bashrc
+echo "export PATH=${PATH}" >> ~/.bashrc
+echo "export SUPPORT=${SUPPORT}" >> ~/.bashrc
+#source ~/.bashrc
+
+# Build epics base
+sudo apt-get install build-essential cmake
 cd ${EPICS_BASE}
 make
 
+# Configure folders in epics base
 echo "SUPPORT=$SUPPORT" >> configure/RELEASE
 echo '-include $(TOP)/configure/SUPPORT.$(EPICS_HOST_ARCH)' >> configure/RELEASE
 echo "EPICS_BASE=$EPICS_BASE" >> configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE' >> configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE.$(EPICS_HOST_ARCH)' >> configure/RELEASE
 
-sudo apt-get libtirpc-dev re2c
+# Clone and prepaere synapps modules chosen in assemble_synapps
 cd ${SCRIPTS_DIR}
 chmod +x assemble_synApps
 perl assemble_synApps
-cd support
+
+# Build synApps
+sudo apt-get install libtirpc-dev re2c
+cd ${SUPPORT}
 make
