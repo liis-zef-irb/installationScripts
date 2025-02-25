09f347b2915682789f385f89a52e51fd916e3664
Lovro <lovro.basioli@irb.hr>
2025-02-25 14:27:16 +0100

    VCOM driver attempt

---
+++ InstallVcomDriver.sh
@@ -0,0 +1,24 @@
+# Prepare driver files with compability modification
+unzip Advantech-VCOM-Linux-Driver-2.3.5.zip
+scp adv_uart.c Advantech-VCOM-Linux-Driver-2.3.5/driver/adv_uart.c
+
+# Prepare LIIS device mapping
+scp advttyd.conf Advantech-VCOM-Linux-Driver-2.3.5/config/advttyd.conf
+
+# Make driver
+sudo apt-get install linux-headers-generic dkms openssl libssl-dev
+cd Advantech-VCOM-Linux-Driver-2.3.5/
+make
+
+# Install driver
+sudo mkdir /usr/local/advtty
+sudo make install
+
+# start the daemon
+sudo advman -o insert
+sudo advman -o start
+
+# (auto)start advvcom service
+sudo make install -C ./misc/systemd/
+systemctl enable advvcom.service
+systemctl start advvcom.service
