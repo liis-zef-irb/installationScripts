269eb790dd48907a05136fde9383044992334495
redomitro <rea.domitrovic@irb.hr>
2026-06-17 18:05:22 +0200

    Added copying and reference to synApps_libi, we need that

--- install_synApps.sh
+++ install_synApps.sh
@@ -5,8 +5,8 @@ echo "EPICS_BASE=$EPICS_BASE" >> $EPICS_BASE/configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE' >> $EPICS_BASE/configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE.$(EPICS_HOST_ARCH)' >> $EPICS_BASE/configure/RELEASE
 
-# Clone and prepare synapps modules chosen in assemble_synapps
-scp ./assemble_synApps ${EPICS_ROOT}/assemble_synApps
+# Clone and prepare synapps modules chosen in synApps_libi.txt
+scp ./assemble_synApps ./synApps_libi.txt ${EPICS_ROOT}
 cd ${EPICS_ROOT}
 chmod +x assemble_synApps
 perl assemble_synApps --config=synApps_libi.txt
