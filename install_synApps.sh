f19efdcd6843206a24aaa2618960765e01537506
redomitro <rea.domitrovic@irb.hr>
2026-07-14 14:36:06 +0200

    fixed bug in install synApps

--- install_synApps.sh
+++ install_synApps.sh
@@ -6,7 +6,8 @@ echo '-include $(TOP)/configure/EPICS_BASE' >> $EPICS_BASE/configure/RELEASE
 echo '-include $(TOP)/configure/EPICS_BASE.$(EPICS_HOST_ARCH)' >> $EPICS_BASE/configure/RELEASE
 
 # Clone and prepare synapps modules chosen in synApps_libi.txt
-scp ./assemble_synApps ./synApps_libi.txt ${EPICS_ROOT}
+scp ./assemble_synApps ${EPICS_ROOT}
+scp ./synApps_libi.txt ${EPICS_ROOT}
 cd ${EPICS_ROOT}
 chmod +x assemble_synApps
 perl assemble_synApps --config=synApps_libi.txt
